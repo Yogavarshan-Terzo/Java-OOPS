25cacc1717b26534166a31efda1e64520631c001
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-14 15:21:11 +0530

    Implement-Encapsulation

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student yoga = new Student("yogavarshan", 20, "Varusanadu,Theni");
+        System.out.println(yoga.toString());
     }
 }
